34da585b6cfdf8372906d05d98da88832bff4af8
axl.dm82 <axl.dm82@gmail.com>
2024-04-02 15:30:15 +0200

    add initial files for server

---
+++ server_postgres/init_postgres/init.sql
@@ -0,0 +1,77 @@
+-- use postgis to manipule geometries
+CREATE EXTENSION IF NOT EXISTS postgis;
+
+
+-- ------------------------------------------------------------------ --
+-- --  Tables and constraints                                      -- --
+-- ------------------------------------------------------------------ --
+
+CREATE TABLE public.sequence
+(
+	id CHARACTER VARYING(256) NOT NULL,
+	`date` DATE NOT NULL,
+	
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE public.photo
+(
+	id CHARACTER VARYING(256) NOT NULL,
+	id_sequence CHARACTER VARYING(256) NOT NULL,
+	geom GEOMETRY (POINT, 4326) NOT NULL,
+	azimut FLOAT NOT NULL,
+	width INTEGER NOT NULL,
+	height INTEGER NOT NULL,
+	
+	PRIMARY KEY (id),
+	FOREIGN KEY (id_sequence)
+		REFERENCES public.sequence (id) MATCH SIMPLE
+		ON UPDATE NO ACTION
+		ON DELETE NO ACTION
+		NOT VALID
+);
+
+ALTER TABLE public.photo
+	OWNER to postgres;
+
+
+CREATE TABLE public.panneau
+(
+	id SERIAL NOT NULL,
+	geom GEOMETRY(POINT, 4326) NOT NULL,
+	`size` FLOAT NOT NULL,
+	orientation FLOAT NOT NULL, 
+	precision FLOAT NOT NULL,
+	
+	PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public.panneau
+	OWNER to postgres;
+
+
+CREATE TABLE public.imagette
+(
+	id CHARACTER VARYING(256) NOT NULL,
+	id_photo CHARACTER VARYING NOT NULL,
+	id_panneau CHARACTER VARYING DEFAULT NULL,
+	x FLOAT NOT NULL,
+	y FLOAT NOT NULL,
+	dz FLOAT NOT NULL,
+	
+	PRIMARY KEY(id),
+	FOREIGN KEY (id_photo)
+		REFERENCES public.photo (id) MATCH SIMPLE
+		ON UPDATE NO ACTION
+		ON DELETE NO ACTION
+		NOT VALID,
+	FOREIGN KEY (id_panneau)
+		REFERENCES public.panneau (id) MATCH SIMPLE
+		ON UPDATE NO ACTION
+		ON DELETE NO ACTION
+		NOT VALID
+);
+
+ALTER TABLE IF EXISTS public.imagette
+	OWNER to postgres;
+
