adf4c2ca24ee0fe0d4e73cd2884ffb4545ceac22
Sivloc <clovis.bergeret@laposte.net>
2024-04-03 10:19:59 +0200

    Patch test_center

--- test_center.py
+++ test_center.py
@@ -6,12 +6,21 @@ def test_get_whxy():
     assert extraction.get_whxy_from_img_path('data_test/testB14.jpg') == (11000, 5500, 5952, 2720)
     assert extraction.get_whxy_from_img_path('data_test/test_C12.jpg') == (11000, 5500, 7104, 2416)
 
-def test_find_sign():
-    shape,contour = moteur.find_sign('data_test/test_A14.jpg')
+def test_find_sign():    
+    img = cv2.imread('data_test/test_A14.jpg')
+    gray = moteur.BGRtoGRAY(img)
+    edges = moteur.DetectionContours(gray)
+    shape,contour = moteur.find_sign(img,edges)
     assert shape == 'triangle'
-    shape,contour = moteur.find_sign('data_test/test_B14.jpg')
+    img = cv2.imread('data_test/testB14.jpg')
+    gray = moteur.BGRtoGRAY(img)
+    edges = moteur.DetectionContours(gray)
+    shape,contour = moteur.find_sign(img,edges)
     assert shape == 'cercle'
-    shape,contour = moteur.find_sign('data_test/test_C12.jpg')
+    img = cv2.imread('data_test/test_C12.jpg')
+    gray = moteur.BGRtoGRAY(img)
+    edges = moteur.DetectionContours(gray)
+    shape,contour = moteur.find_sign(img,edges)
     assert shape == 'carre'
 
 if __name__ == "__main__":
