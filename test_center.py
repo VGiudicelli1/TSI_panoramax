031bb0536c1cb81114a73cd77212ba6780cf7ff6
Sivloc <clovis.bergeret@laposte.net>
2024-04-03 10:12:25 +0200

    Testing workflows

--- test_center.py
+++ test_center.py
@@ -2,18 +2,18 @@ import extraction
 import moteur
 
 def test_get_whxy():
-    assert get_whxy_from_img_path('test_A14.jpg') == (11000, 5500, 5440, 2384)
-    assert get_whxy_from_img_path('testB14.jpg') == (11000, 5500, 5952, 2720)
-    assert get_whxy_from_img_path('test_C12.jpg') == (11000, 5500, 7104, 2416)
+    assert extraction.get_whxy_from_img_path('data_test/test_A14.jpg') == (11000, 5500, 5440, 2384)
+    assert extraction.get_whxy_from_img_path('data_test/testB14.jpg') == (11000, 5500, 5952, 2720)
+    assert extraction.get_whxy_from_img_path('data_test/test_C12.jpg') == (11000, 5500, 7104, 2416)
 
 def test_find_sign():
-    shape,contour = find_sign('test_A14.jpg')
+    shape,contour = moteur.find_sign('data_test/test_A14.jpg')
     assert shape == 'triangle'
-    shape,contour = find_sign('test_B14.jpg')
+    shape,contour = moteur.find_sign('data_test/test_B14.jpg')
     assert shape == 'cercle'
-    shape,contour = find_sign('test_C12.jpg')
+    shape,contour = moteur.find_sign('data_test/test_C12.jpg')
     assert shape == 'carre'
-    
+
 if __name__ == "__main__":
     import pytest
     pytest.main(["-v", __file__])
