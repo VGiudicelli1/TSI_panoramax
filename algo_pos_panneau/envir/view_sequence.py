e04fb53c7f883a59745962e86dfc60240bf81d3c
VGiudicelli <vincent.giudicelli@ensg.eu>
2024-04-04 09:48:22 +0200

    update displaying data

--- algo_pos_panneau/envir/view_sequence.py
+++ algo_pos_panneau/envir/view_sequence.py
@@ -1,6 +1,7 @@
 import utils
 import storage
 import matplotlib.pyplot as plt
+import numpy as np
 """
 Affiche les données de la séquence:
 - photos,
@@ -15,7 +16,7 @@ utils.proj_geo_to_lambert_delta(photo, delta=(0,0))
 utils.proj_geo_to_lambert_delta(imagette, delta=(0,0))
 utils.proj_geo_to_lambert_delta(panneau, delta=(0,0))
 
-imagette_photo = imagette.join(photo.set_index("id").add_prefix("source_"), on="id_photo")
+imagette_photo_panneau = imagette.join(photo.set_index("id").add_prefix("source_"), on="id_photo").join(panneau.set_index("id").add_prefix("dest_"), on="id_panneau")
 
 plt.axis("equal")
 plt.title(f"Données de la séquence {id_sequence}\n(coordonnées Lambert)")
@@ -23,8 +24,21 @@ plt.plot(list(photo.e),    list(photo.n),    marker="o", lw=0, color="blue",   l
 plt.plot(list(imagette.e), list(imagette.n), marker="x", lw=0, color="green",  label=f"imagette ({len(imagette)})")
 plt.plot(list(panneau.e),  list(panneau.n),  marker="o", lw=0, color="orange", label=f"panneau ({len(panneau)})"  )
 
-for i_img in imagette_photo.index:
-    img = imagette_photo.loc[i_img]
-    plt.plot([img.e, img.source_e], [img.n, img.source_n], lw=0.2, color="blue")
+X = np.zeros((3 * len(imagette_photo_panneau), ))
+X[0::3] = imagette_photo_panneau.e
+X[2::3] = None
+
+Y = np.zeros((3 * len(imagette_photo_panneau), ))
+Y[0::3] = imagette_photo_panneau.n
+Y[2::3] = None
+
+X[1::3] = imagette_photo_panneau.source_e
+Y[1::3] = imagette_photo_panneau.source_n
+plt.plot(X, Y, lw=0.2, color="blue")
+
+X[1::3] = imagette_photo_panneau.dest_e
+Y[1::3] = imagette_photo_panneau.dest_n
+plt.plot(X, Y, lw=0.2, color="orange")
+
 plt.legend()
 plt.show()
