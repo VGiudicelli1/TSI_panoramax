4da911ee91d9bc5267092f47e70adc0851e19a49
MatRouillard <mathis.rouillard@ensg.eu>
2024-04-05 15:45:55 +0200

    Update utils files and main files / add data

--- python/search_api/api.py
+++ python/search_api/api.py
@@ -1,143 +1,10 @@
 import requests
 import shutil
-import json
-import psycopg2
-import pandas as pd
+from PIL import Image
+from io import BytesIO
 
 log = print
 
-def getConnection(host,
-				  user="postgres",
-				  password="postgres",
-				  port="5432",
-				  database="panneaux"):
-	"""
-	Get connection token to the database.
-	Only the host is required, other parameters can be ommited.
-
-	Parameters
-	----------
-	host : str
-		Ip address for the database connection.
-	user : str, optional
-		Username for the database connection. The default is "postgres".
-	password : str, optional
-		Password for the database connection. The default is "postgres".
-	port : str, optional
-		Port for the connection. The default is "5432".
-	database : str, optional
-		Database to connect to. The default is "panoramax".
-
-	Returns
-	-------
-	connection : psycopg2.extensions.connection
-		Database connection token.
-
-	"""
-	connection = psycopg2.connect(host=host,
-								  user=user,
-								  password=password,
-								  port=port,
-								  database=database)
-	return connection
-
-def insertQuery(connection, query):
-	"""
-	Insert a row in a database, where the query is already formated.
-	It returns the number of lines affected by the query.
-
-	Parameters
-	----------
-	connection : psycopg2.extensions.connection
-		Database connection token.
-	query : str
-		Insert query already formatted.
-
-	Returns
-	-------
-	linesNumber : int
-		Number of lines affected by the query.
-
-	"""
-	linesNumber= 0
-	try:
-		cursor = connection.cursor()
-		# Execute and commit the query
-		cursor.execute(query)
-		connection.commit()
-		linesNumber = cursor.rowcount
-	except Exception as e:
-	    # If there is an error, the transaction is canceled
-	    connection.rollback()
-	    log("The following error occured :", e)
-	finally:
-		# The transaction is closed anyway
-		cursor.close()
-		return linesNumber
-
-
-def readCSVFile(path):
-	"""
-	Read a CSV file into a pandas dataframe
-
-	Parameters
-	----------
-	path : str
-		Relatiive path of the file.
-
-	Returns
-	-------
-	dataframe : pandas.dataframe.
-		Dataframe of the csv file
-
-	"""
-	data = pd.read_csv(path, delimiter = ";")
-	return data
-
-def extractCodeAndValue(row):
-	"""
-	Extract the code of the sign and the value if necessary.
-
-	Parameters
-	----------
-	row : pandas.core.series.Series
-		Line of the read csv file.
-
-	Returns
-	-------
-	code : str
-		Code of the sign.
-	value : str
-		None if the sign has no value. Otherwise, correspond to the sign value
-
-	"""
-	code = row["directory"]
-	if "B14" in code:
-		value = code[4::]
-		code = code[0:3]
-	else:
-		value = "null"
-	return code, value
-
-def reformatBbox(row):
-	"""
-	Reformat the bbox in the appropriate format.
-
-	Parameters
-	----------
-	row : pandas.core.series.Series
-		Line of the read csv file.
-
-	Returns
-	-------
-	bbox : str
-		Bbox of the sign in the picture.
-
-	"""
-	comment = json.loads(row["comment"])
-	[x, y, w, h] = list(comment["xywh"])
-	bbox = "[{},{},{},{}]".format(x, y, x + w, y + h)
-	return bbox
 
 def getClosestPictureInformation(lon, lat):
 	"""
@@ -158,41 +25,44 @@ def getClosestPictureInformation(lon, lat):
 		Information about the picture.
 	"""
 	url = "https://api.panoramax.xyz/api/search"
-	
+	# url = "https://panoramax.ign.fr/api/search"
+
 	# Data for the post request
 	dataToSend = {
-		"limit":1,
+		"limit": 1,
 	}
-	
-	bbox = "{},{},{},{}".format(round(lon - 0.00001, 5),
-							 round(lat - 0.00001, 5),
-							 round(lon + 0.00001, 5),
-							 round(lat + 0.00001, 5))
-	
+
+	# Bbox centered on the picture
+
+	bbox = "{},{},{},{}".format(lon - 0.00001,
+								lat - 0.00001,
+								lon + 0.00001,
+								lat + 0.00001)
 	dataToSend["bbox"] = bbox
+
 	log(url+"?limit=1&bbox={}".format(bbox))
-	
+
 	# Send and get the answer of the file
 	responseTotal = requests.post(url, json=dataToSend)
 	response = responseTotal.json()
-	
+
 	# If there is no answer, an exception is raised
 	if len(response["features"]) == 0:
 		raise requests.exceptions.RequestException()
-	
-	
+
 	# Usefull information
 	info = {
-		"sequence_id":response["features"][0]["collection"],
-		"picture_id":response["features"][0]["id"],
-		"lon_picture":response["features"][0]["geometry"]["coordinates"][0],
-		"lat_picture":response["features"][0]["geometry"]["coordinates"][1],
-		"is360picture":""
+		"collectionId": response["features"][0]["collection"],
+		"pictureId": response["features"][0]["id"],
+		"lonPicture": response["features"][0]["geometry"]["coordinates"][0],
+		"latPicture": response["features"][0]["geometry"]["coordinates"][1],
+		"is360picture": ""
 	}
-	
+
 	info["is360picture"] = is360Picture(response["features"][0]["properties"])
-	
+
 	return info
+
 
 def is360Picture(properties):
 	"""
@@ -211,25 +81,52 @@ def is360Picture(properties):
 	"""
 	# We initiate the test to false
 	test = False
-	
+
 	# Try and catch is necessary because the field of view is not always in the data
 	try:
 		if properties["pers:interior_orientation"]["field_of_view"] == 360:
 			test = True
 	finally:
 		return test
-	
 
-def savePicture(sequence_id, picture_id, dest):
+def getShapePicture(pictureId):
+	"""
+	Get the shape of a picture (width, height).
+	It uses the Pictures API of Panoramax
+
+	Parameters
+	----------
+	pictureId : str
+		Id of the picture.
+
+	Returns
+	-------
+	None.
+
+	"""
+	url = "https://api.panoramax.xyz/api/pictures/{}/hd.jpg".format(pictureId)
+	response = requests.get(url, stream=True)
+
+	# Check the status code
+	if response.status_code == 200:
+		# Get the picture without saving it
+	    picture = Image.open(BytesIO(response.content))
+	    
+	    # Get the size of the picture
+	    width, height = picture.size
+	
+	return (width, height)
+
+def savePicture(pictureId, dest):
 	"""
 	Save the picture in HD in the destination folder.
 	It also saves the metadata linked to the image.
 
 	Parameters
 	----------
-	sequence_id : str
-		Id of the sequence.
-	picture_id : str
+	collectionId : str
+		Id of the collection.
+	pictureId : str
 		Id of the picture.
 	dest : str
 		Folder where the picture will be save.
@@ -240,24 +137,33 @@ def savePicture(sequence_id, picture_id, dest):
 
 	"""
 	# Get the picture
-	picture_url = "https://api.panoramax.xyz/api/pictures/{}/hd.jpg".format(picture_id)
-	picture_response = requests.get(picture_url, stream = True)
-	
-	# Save the picture 
-	if picture_response.status_code == 200:
-		with open(dest+"/{}.jpeg".format(picture_id),'wb') as f:
-			shutil.copyfileobj(picture_response.raw, f)
-			log("Picture {} saved in the following folder : \n{}\n".format(picture_id + ".jpeg", dest))
+	pictureUrl = "https://api.panoramax.xyz/api/pictures/{}/hd.jpg".format(pictureId)
+	pictureResponse = requests.get(pictureUrl, stream=True)
+
+	# Save the picture
+	if pictureResponse.status_code == 200:
+		with open(dest+"/{}.jpeg".format(pictureId), 'wb') as f:
+			shutil.copyfileobj(pictureResponse.raw, f)
+			log("Picture {} saved in the following folder : \n{}\n"
+				.format(pictureId + ".jpeg", dest))
 
 
-if __name__=="__main__":
-	try:
-		response = getClosestPictureInformation(4.83534, 45.75564)
-		response = getClosestPictureInformation(2.559261111, 48.843405556)
-	except:
-		log("pb")
-	response = getClosestPictureInformation(7.70989, 48.536979)
+def savePictures(collectionId, pictureId, dest):
+	"""
+	Save the picture, the next and the previous one only if they exist.
+	If they do not exist, none of them will be downloaded.
 
+	Parameters
+	----------
+	collectionId : str
+		Id of the collection.
+	pictureId : str
+		Id of the picture.
+	dest : str
+		Folder where the picture will be save.
 
-if __name__=="__main__":
-	dataframe = readCSVFile("../../exif_data.csv")
+	Returns
+	-------
+	None.
+
+	"""
