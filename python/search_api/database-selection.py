a79d7fc8e311d35b76305fa828f07ff3e7f18d1c
MatRouillard <mathis.rouillard@ensg.eu>
2024-04-16 11:00:27 +0200

    Add function to update database

--- python/search_api/database-selection.py
+++ python/search_api/database-selection.py
@@ -3,21 +3,27 @@ import database as db
 
 if __name__ == "__main__":
 	# Get the connection
-	connection = db.getConnection("127.0.0.1")
+	# Mutual database connection :  "172.31.58.179", password = "tsi23lesboss",
+	connection = db.getConnection("127.0.0.1", 
+							   database="panoramax")
 	
 	# Select all lines and get the column names
-	cursor = db.selectAll(connection, "panneaux")
+	# cursor = db.selectAll(connection, "cropped_sign")
+	
+	
+	cursor = db.selectCroppedSignByFilename(connection, "01d7d4010541dfc5eaa5b6d87b58ec5efca8bfcf04a79e529b42f0702ef9a4fd.jpg")
 	
 	columns = [desc[0] for desc in cursor.description]
 	
-	for row in cursor:
-
-		filename = db.getValueAtColumn(row, columns, "filename")
-		code = db.getValueAtColumn(row, columns, "code")
-		if "B14" in code:
-			path = db.getPathFromRow(row, columns)
-			break
-
-	print(db.itemAlreadyInDatabase(connection, 1, "panneaux"))
+	if cursor.rowcount == 0:
+		print("No cropped sign")
+	elif cursor.rowcount > 1:
+		print("Too many cropped sign")
+	else:
+		row = cursor.fetchone()
+		id = db.getValueAtColumn(row, columns, "id")
+		lines = db.updateCenterOfSign(connection, id, 1125, 125, 60)
+		print(lines)
+	
 	
 	
